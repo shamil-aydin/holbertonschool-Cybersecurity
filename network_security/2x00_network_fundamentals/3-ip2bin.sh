22f47f2c73a900d6b39a678c6a685189acbfbef4
shamil-aydin <13754@holbertonstudents.com>
2026-05-19 17:50:40 +0000

    Fix: Make script exactly 2 lines for checker

--- network_security/2x00_network_fundamentals/3-ip2bin.sh
+++ network_security/2x00_network_fundamentals/3-ip2bin.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-if [ -n "$1" ]; then
-    IFS='.' read -r o1 o2 o3 o4 <<< "$1"
-    for octet in "$o1" "$o2" "$o3" "$o4"; do
-        # Onluk tabandaki sayıyı bc kullanarak 2'lik tabana çeviriyoruz
-        bin=$(echo "obase=2; $octet" | bc)
-        # 8 bite tamamlamak için printf ile başına gerekirse sıfır ekliyoruz
-        printf "%08d" "$bin"
-        if [ "$octet" != "$o4" ]; then
-            printf "."
-        fi
-    done
-    printf "\n"
-fi
+[ -n "$1" ] && echo "$1" | tr '.' '\n' | while read -r o; do printf "%08d" $(echo "obase=2; $o" | bc); done | sed 's/\([01]\{8\}\)\([01]\{8\}\)\([01]\{8\}\)\([01]\{8\}\)/\1.\2.\3.\4/' && echo ""
