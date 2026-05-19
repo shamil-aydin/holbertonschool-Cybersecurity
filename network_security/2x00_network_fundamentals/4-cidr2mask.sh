475caf9219b6bf0f51591c46bf7d71fd1e08ec56
shamil-aydin <13754@holbertonstudents.com>
2026-05-19 17:53:48 +0000

    Fix: Strict 2-line version for checker

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-[ -z "$1" ] && exit
-mask=$((0xFFFFFFFF << (32 - $1)))
-printf "%d.%d.%d.%d" $(( (mask >> 24) & 255 )) $(( (mask >> 16) & 255 )) $(( (mask >> 8) & 255 )) $(( mask & 255 ))
+mask=$((0xFFFFFFFF << (32 - $1))); printf "%d.%d.%d.%d\n" $(( (mask >> 24) & 255 )) $(( (mask >> 16) & 255 )) $(( (mask >> 8) & 255 )) $(( mask & 255 ))
