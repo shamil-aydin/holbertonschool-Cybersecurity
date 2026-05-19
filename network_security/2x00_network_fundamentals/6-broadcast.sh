959fe6ebcd185179ed1b17ca5644e4cca06ca71f
shamil-aydin <13754@holbertonstudents.com>
2026-05-19 17:58:17 +0000

    Fix: Remove newline from broadcast script for exact character count

--- network_security/2x00_network_fundamentals/6-broadcast.sh
+++ network_security/2x00_network_fundamentals/6-broadcast.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-IFS=. read -r i1 i2 i3 i4 <<< "$1"; IFS=. read -r m1 m2 m3 m4 <<< "$2"; printf "%d.%d.%d.%d\n" $((i1|(255-m1))) $((i2|(255-m2))) $((i3|(255-m3))) $((i4|(255-m4)))
+IFS=. read -r i1 i2 i3 i4 <<< "$1"; IFS=. read -r m1 m2 m3 m4 <<< "$2"; printf "%d.%d.%d.%d" $((i1|(255-m1))) $((i2|(255-m2))) $((i3|(255-m3))) $((i4|(255-m4)))
