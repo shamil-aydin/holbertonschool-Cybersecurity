f98dcb995b56169d6e1a3e9df469f9afa7fd375f
shamil-aydin <13754@holbertonstudents.com>
2026-05-19 17:53:10 +0000

    Fix: Remove trailing newline to match length

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-[ -n "$1" ] && mask=$((0xFFFFFFFF << (32 - $1))) && printf "%d.%d.%d.%d\n" $(( (mask >> 24) & 255 )) $(( (mask >> 16) & 255 )) $(( (mask >> 8) & 255 )) $(( mask & 255 ))
+[ -z "$1" ] && exit
+mask=$((0xFFFFFFFF << (32 - $1)))
+printf "%d.%d.%d.%d" $(( (mask >> 24) & 255 )) $(( (mask >> 16) & 255 )) $(( (mask >> 8) & 255 )) $(( mask & 255 ))
