18ca45d210eba8f02a77cfe89285d5818cce34aa
shamil-aydin <13754@holbertonstudents.com>
2026-05-19 17:59:55 +0000

    Task 7: Fix line count to exactly 2 lines

--- network_security/2x00_network_fundamentals/7-host_range.sh
+++ network_security/2x00_network_fundamentals/7-host_range.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-mask=$((0xFFFFFFFF << (32 - $2))); net=$(( ( $(printf "%d\n" ${1//./ }) ) & mask )); broad=$(( net | ~mask & 0xFFFFFFFF ))
-printf "%d.%d.%d.%d - %d.%d.%d.%d\n" $(( (net+1) >> 24 & 255 )) $(( (net+1) >> 16 & 255 )) $(( (net+1) >> 8 & 255 )) $(( (net+1) & 255 )) $(( (broad-1) >> 24 & 255 )) $(( (broad-1) >> 16 & 255 )) $(( (broad-1) >> 8 & 255 )) $(( (broad-1) & 255 ))
+IFS=. read -r a b c d <<< "$1"; mask=$((0xFFFFFFFF << (32 - $2))); net=$(( ( (a<<24) + (b<<16) + (c<<8) + d ) & mask )); broad=$(( net | ~mask & 0xFFFFFFFF )); printf "%d.%d.%d.%d - %d.%d.%d.%d\n" $(( (net+1) >> 24 & 255 )) $(( (net+1) >> 16 & 255 )) $(( (net+1) >> 8 & 255 )) $(( (net+1) & 255 )) $(( (broad-1) >> 24 & 255 )) $(( (broad-1) >> 16 & 255 )) $(( (broad-1) >> 8 & 255 )) $(( (broad-1) & 255 ))
